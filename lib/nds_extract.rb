7e5d8980b4aedde95dcb179eec400981b0aa22d5
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-19 00:46:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,12 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  directors = []
+  director_index = 0
+  while director_index < source.length do
+    directors << source[director_index]# Write this implementation
+  end
+
 end
 
 def total_gross(source)
@@ -39,5 +44,3 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 end
-
-
